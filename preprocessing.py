3fdd2141d7a43fd377e6f20eff73153a37da0ef8
miljan <miljan.martic@gmail.com>
2015-04-08 22:54:51 +0200

    -data included -added initial data preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,38 @@
+import csv
+import numpy as np
+from datetime import datetime
+from pprint import pprint
+from itertools import islice
+
+
+__author__ = 'miljan'
+
+
+def read_data():
+    with open('./data/tweets.tsv', 'rU') as file1:
+        # filter removes the rows starting with # (comments)
+        file_reader = csv.reader(filter(lambda x: x[0] != '#', file1), delimiter='\t', dialect=csv.excel_tab)
+        # skip header
+        file_reader.next()
+
+        # matrix containing all 8 ranking for each tweet
+        rating_matrix = []
+        data_matrix = []
+        for row in file_reader:
+            data_matrix.append([row[0]] + [process_timestamp(row[1])] + row[2:5])
+            rating_matrix.append(row[5:])
+
+        # replace all missing values with 0
+        rating_matrix = [[x if x is not '' else '0' for x in y] for y in rating_matrix]
+        # convert to numpy ints
+        rating_matrix = np.array(rating_matrix, dtype='int')
+
+    return data_matrix, rating_matrix
+
+
+def process_timestamp(timestamp):
+    return datetime.strptime(timestamp, '%m/%d/%y %H:%M')
+
+
+if __name__ == '__main__':
+    data_matrix, rating_matrix = read_data()
