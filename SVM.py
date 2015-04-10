480fcb0d0dd58230512a10c64acc39602da488df
bojanvujatovic <bojan.vujatovic@gmail.com>
2015-04-10 20:14:44 +0100

    added feature: length, number in tweet, is a word present

--- SVM.py
+++ SVM.py
@@ -11,6 +11,7 @@ from preprocessing import *
 from itertools import product
 
 from sklearn.feature_extraction.text import CountVectorizer
+from enable.savage.svg.css.values import length
 
 if __name__ == '__main__':
     random.seed(42)
@@ -21,13 +22,16 @@ if __name__ == '__main__':
     stemmings = [True, False]
     TFIDFs = [False, True]
     stop_wordss = [None, 'english']
-    max_featuress = np.linspace(10, 5000, 5)
-    ngram_ranges = [(1, 1), (1, 2)]
+    max_featuress = np.linspace(10, 5000, 5).tolist() + [None]
+    ngram_ranges = [(1, 2)]
+    lengths = [True, False]
+    number_in_tweets = [True, False]
+    words_presents = [['obama', 'mccain'], []]
     
+    parameter_space = product(stemmings, TFIDFs, stop_wordss, max_featuress, ngram_ranges, lengths, number_in_tweets, words_presents)
     
-    parameter_space = product(stemmings, TFIDFs, stop_wordss, max_featuress, ngram_ranges)
-    
-    for stemming, TFIDF, stop_words, max_features, ngram_range in parameter_space:
+    # Search the highest F1 macro score through parameter space 
+    for stemming, TFIDF, stop_words, max_features, ngram_range, length, number_in_tweet, words_present in parameter_space:
             
         # Loading dataset
         data_matrix, rating_matrix = read_data()
@@ -39,7 +43,10 @@ if __name__ == '__main__':
                                                           TFIDF=TFIDF, 
                                                           stop_words=stop_words, 
                                                           max_features=int(max_features), 
-                                                          ngram_range=ngram_range)
+                                                          ngram_range=ngram_range, 
+                                                          length=length, 
+                                                          number_in_tweet=number_in_tweet, 
+                                                          words_present=words_present)
         
         # majority voting rating
         majority_rating = majority_voting_ratings(rating_matrix)
@@ -48,22 +55,28 @@ if __name__ == '__main__':
             rating_pred = []
             rating_real = []
             
+            # Do Kfold cross validation
             fold = 0
             for train, val in KFold(N_tweets, n_folds=20):
                 fold += 1
                 print fold 
                 
+                # Separate dataset into training and validation
                 X_train, X_val, y_train, y_val = transformed_tweets[train], transformed_tweets[val], majority_rating[train], majority_rating[val]
                 
+                # Fit the SVM model
                 clf = svm.SVC(C=C)
                 clf.fit(X_train, y_train)
                 
+                # Predict the validation ratings
                 rating_pred = np.append(rating_pred, clf.predict(X_val))
                 rating_real = np.append(rating_real, y_val)
             
+            # Calculate the confusion matrix and f1 macro score
             cm = confusion_matrix(rating_real, rating_pred)
             f1_score_macro = f1_score(rating_real, rating_pred, average='macro')
             
+            # Print the results
             print "C =", C, ", stemming = ", stemming, ", TFIDF =", TFIDF, ", stop_words = ", stop_words, ", max_features = ", max_features, ", ngram_range = ", ngram_range
             #print cm
             print f1_score_macro
--- preprocessing.py
+++ preprocessing.py
@@ -47,21 +47,52 @@ def argmax(arr):
 def process_timestamp(timestamp):
     return datetime.strptime(timestamp, '%m/%d/%y %H:%M')
 
+def list_of_ints_from_string(s):
+    l = []
+    for t in word_tokenize(s.decode("utf8")):
+        try:
+            l.append(int(float(t)))
+        except ValueError:
+            pass
+        
+    return l
 
-
-def cleaned_bag_of_words_dataset(data_matrix, stemming=False, stop_words=None, TFIDF=False, ngram_range=(1, 1), max_features=None):
+def cleaned_bag_of_words_dataset(data_matrix, stemming=False, stop_words=None, TFIDF=False, ngram_range=(1, 1), max_features=None,
+                                 length=False, number_in_tweet=False, words_present=[]):
     if stemming:
         stemmer = SnowballStemmer("english")
-        tweets = [" ".join([stemmer.stem(word) for word in word_tokenize(data_point[2].decode("utf8"))]) for data_point in data_matrix]
+        tweets = [" ".join([stemmer.stem(word) for word in word_tokenize(data_point[2].lower().decode("utf8"))]) for data_point in data_matrix]
     else:
-        tweets = [data_point[2] for data_point in data_matrix]
+        tweets = [data_point[2].lower() for data_point in data_matrix]
         
     if TFIDF:
         vectorizer = TfidfVectorizer(stop_words=stop_words, ngram_range=ngram_range, max_features=max_features)
     else:
         vectorizer = CountVectorizer(stop_words=stop_words, ngram_range=ngram_range, max_features=max_features)
     
-    return vectorizer.fit_transform(tweets)
+    dataset = vectorizer.fit_transform(tweets).toarray()
+    
+    if length:
+        lengths = np.array([[len(word_tokenize(data_point[2].decode("utf8")))] for data_point in data_matrix])
+        dataset = np.concatenate((dataset, lengths), axis=1)
+     
+    if number_in_tweet:
+        numbers = []
+        for data_point in data_matrix:
+            number_list = list_of_ints_from_string(data_point[2])
+            filtered_number_list = [number for number in number_list if abs(number) < 10]
+            if len(filtered_number_list) == 0:
+                numbers.append([0])
+            else:
+                numbers.append([np.mean(filtered_number_list)])
+        dataset = np.concatenate((dataset, numbers), axis=1)
+
+    for word in words_present:
+        word_present = np.array([[int(word.lower() in word_tokenize(data_point[2].lower().decode("utf8")))] for data_point in data_matrix])
+        dataset = np.concatenate((dataset, word_present), axis=1)
+        
+    return dataset
+
 
 def majority_voting_ratings(rating_matrix):
     majority_rating = []
